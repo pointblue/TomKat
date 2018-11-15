7af44c3ec9cc40509d86149bbf1e9a37502e972c
Kristen Dybala <kdybala@gmail.com>
2018-11-14 17:58:57 -0800

    add index script that will be used to call others

---
+++ code/01_index.R
@@ -0,0 +1,10 @@
+# README--------------
+# Master script file
+
+
+# VEGETATION-----------
+# process & clean data (create master veg file)
+source('code/vegetation_prep.R')
+
+# produce current veg map (veg map 1)
+source('code/vegetation_map1.R')
