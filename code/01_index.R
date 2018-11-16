647dddade48644aacf2cdd744d0c7bdfd46c0c43
Kristen Dybala <kdybala@gmail.com>
2018-11-15 17:16:24 -0800

    update index.html with new locations for widgets and Rmd

--- code/01_index.R
+++ code/01_index.R
@@ -7,13 +7,14 @@
 source('code/vegetation_prep.R')
 
 # produce current veg map (veg map 1) - but manually export map?!
-# source('docs/vegetation_map1.R') 
+# source('widgets/vegetation_map1.R') 
 
 # produce net change from 2012 map (veg map 2)
-# source('docs/vegetation_map2.R')
+# source('widgets/vegetation_map2.R')
 
 # produce ranch-wide trend graphs
-# source('docs/vegetation_graphs.R')
+# source('widgets/vegetation_graphs.R')
 
 # render Rmd to html
-knitr::knit(here::here('vegetation.Rmd'), output = here::here('vegetation.html'))
+knitr::knit(here::here('Rmd/vegetation.Rmd'), 
+            output = here::here('vegetation.html'))
