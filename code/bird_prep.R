00113093b81a93cd4fe8c84aa6d2fbfbcbfcb02b
Kristen Dybala <kdybala@gmail.com>
2019-01-04 12:45:07 -0800

    add testing for expected #species and #surveys

--- code/bird_prep.R
+++ code/bird_prep.R
@@ -52,6 +52,28 @@ dat <- read_csv(here::here(rawdat)) %>% #24752 (older data has more rows?)
 # FYI -- a few points had a 3rd visit in 2011 (?)
 # note: ALHU, RUHU, and SPHU
 
+# check that the number of species and number of surveys hasn't gone down
+#  since last time!
+nspecies <- dat %>%
+  ## exclude unidentified spp, SPHU since ALHU&RUHU are both in there
+  filter(!(substr(Spp, 1, 2) %in% c('UN', 'XX') | Spp == 'SPHU')) %>%
+  select(Spp) %>%
+  distinct() %>% 
+  nrow()
+#91
+
+nsurveys <- dat %>% 
+  unite('id', Point, Visit) %>%
+  select(id) %>%
+  distinct() %>% 
+  nrow()
+#1121
+
+testthat::expect_gte(nspecies, 91)
+testthat::expect_gte(nsurveys, 11121)
+# Note: update these numbers with this year's current number to test it again
+#  next year!
+
 write_csv(dat, here::here(masterdat))
 
 
@@ -65,15 +87,19 @@ sdat <- dat %>%
 
 fdat <- format_data_flat(sdat, strata = 'Point', dist = 'Distance Bin', 
                          dist_bin_id = NULL)
-# 170 strata = 170 unique sampling locations (points), 1-14 visits per sampling location
+# 170 strata = 170 unique sampling locations (points)
+# 1-14 total visits per sampling location
 
-# grsp <- fit_distance_models(data = fdat, spec = 'grsp', maxdist = 150,
-#                             bins = c(0, 20, 40, 60, 80, 100, 150))
+# check for consistent number of sampling points and at least the same max
+#  number of visits per sampling location as last year:
+testthat::expect_equal(fdat$Sample.Label %>% unique() %>% length, 170)
+testthat::expect_gte(fdat$Effort %>% max(), 14)
+
+# fit distance functions and estimate densities per sample point for each
+#  focal species:
 grsp <- fit_distance_models(data = fdat, spec = 'grsp', maxdist = 100,
                             bins = c(0, 30, 50, 70, 80, 90, 100))
 
-# savs <- fit_distance_models(data = fdat, spec = 'savs', maxdist = 150,
-#                             bins = c(0, 30, 50, 70, 100, 150))
 savs <- fit_distance_models(data = fdat, spec = 'savs', maxdist = 100,
                             bins = c(0, 30, 50, 70, 80, 90, 100))
 
@@ -82,14 +108,14 @@ wcsp <- fit_distance_models(data = fdat, spec = 'wcsp', maxdist = 150,
 
 # compile density estimates
 speclist <- c('grsp', 'savs', 'wcsp')
-density <- do.call(rbind.data.frame, lapply(speclist, function(x) {
+
+density <- map_dfr(speclist, function(x) {
   dat = get(tolower(as.character(x)))
   dat = dat$D
   dat$species = toupper(as.character(x))
   return(dat)
-}))
-
-density <- density[-grep('Total',density$Label),]
+}) %>%
+  filter(Label != 'Total')
 
 ## save the results as a CSV file
 write_csv(density, here::here(point_density))
@@ -109,33 +135,38 @@ gdat <- dat %>%
 
 length(unique(gdat$Point)) #104
 
-fdat2 <- format_data_flat(gdat, year = 'Year', dist = 'Distance Bin', 
+fdat2 <- format_data_flat(gdat, 
+                          year = 'Year', 
+                          dist = 'Distance Bin', 
                           dist_bin_id = NULL) 
 # 8 strata (one for each year); 104 unique locations (points); 1-2 visits per point
 
-# grsp2 <- fit_distance_models(data = fdat2, spec = 'grsp', maxdist = 150,
-#                              bins = c(0, 20, 40, 60, 80, 100, 150))
+# check for consistent number of sampling points and at least the same max
+#  number of visits per sampling location as last year:
+testthat::expect_equal(fdat2$Sample.Label %>% unique() %>% length, 104)
+testthat::expect_gte(fdat2$Effort %>% max(), 2)
+
+# fit distance functions and estimate densities per sample point for each
+#  focal species:
 grsp2 <- fit_distance_models(data = fdat2, spec = 'grsp', maxdist = 100,
                              bins = c(0, 30, 50, 70, 80, 90, 100))
 
-# savs2 <- fit_distance_models(data = fdat2, spec = 'savs', maxdist = 150, 
-#                              bins = c(0, 30, 50, 70, 100, 150))
 savs2 <- fit_distance_models(data = fdat2, spec = 'savs', maxdist = 100,
                              bins = c(0, 30, 50, 70, 80, 90, 100))
 
 wcsp2 <- fit_distance_models(data = fdat2, spec = 'wcsp', maxdist = 150, 
                              bins = c(0, 50, 90, 120, 150))
 
-speclist <- c('grsp2', 'savs2', 'wcsp2')
-density2 <- do.call(rbind.data.frame, lapply(speclist, function(x) {
+# compile density estimates
+speclist2 <- c('grsp2', 'savs2', 'wcsp2')
+
+density2 <- map_dfr(speclist2, function(x) {
   dat = get(tolower(as.character(x)))
   dat = dat$D
   dat$species = toupper(as.character(x))
-  dat$species = substr(dat$species, 1, 4)
   return(dat)
-}))
-
-density2 <- density2[-grep('Total', density2$Label),]
+}) %>%
+  filter(Label != 'Total')
 
 ## save the results as a CSV file:
 write_csv(density2, here::here(ranch_density))
@@ -149,18 +180,15 @@ cdat <- dat %>%
   group_by(Point, Spp) %>%
   summarize(Count = sum(Count)) %>%
   spread(key = Spp, value = Count, fill = 0) %>%
-  as.data.frame()
-
-row.names(cdat) <- cdat$Point
-cdat$Point = NULL
+  as.data.frame() %>%
+  column_to_rownames('Point') 
 
 richest <- vegan::estimateR(cdat) %>% as.data.frame() %>%
   mutate(method = row.names(.)) %>%
   gather(`HOCR-001`:`TOKA-178`, key = 'Point', value = 'n') %>%
   filter(method %in% c('S.obs', 'S.ACE')) %>%
   spread(key = method, value = n) %>%
-  mutate(S.ACE = round(S.ACE, digits = 0),
-         prop = S.obs/S.ACE) %>%
+  mutate(S.ACE = round(S.ACE, digits = 0)) %>%
   # add number of visits per point
   left_join(dat %>% select(Point, Visit) %>% distinct() %>% count(Point),
             by = 'Point')
@@ -185,11 +213,8 @@ rdat <- dat %>%
   group_by(id, Spp) %>%
   summarize(Count = sum(Count)) %>%
   spread(key = Spp, value = Count, fill = 0) %>%
-  as.data.frame()
-  
-
-row.names(rdat) <- rdat$id
-rdat$id = NULL
+  as.data.frame() %>%
+  column_to_rownames('id') 
 
 richest2 <- vegan::estimateR(rdat) %>% as.data.frame() %>%
   mutate(method = row.names(.)) %>%
@@ -200,7 +225,5 @@ richest2 <- vegan::estimateR(rdat) %>% as.data.frame() %>%
          max = S.ACE + se.ACE) %>%
   select(id, S.ACE, min, max) %>%
   mutate_at(vars(S.ACE:max), round, digits = 0)
-  mutate(S.ACE = round(S.ACE, digits = 0),
-         prop = S.obs/S.ACE)
 
 write_csv(richest2, here::here(ranch_richness))
