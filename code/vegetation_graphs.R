3f54db8d0f071309e27ff8cdeecbafa3f4d3eb2e
Kristen Dybala <kdybala@gmail.com>
2019-01-07 12:00:04 -0800

    update with new data and switch to plot_ly

--- code/vegetation_graphs.R
+++ code/vegetation_graphs.R
@@ -7,6 +7,7 @@
 ## packages
 library(tidyverse)
 library(sf)
+library(plotly)
 
 ## input files
 masterveg <- 'data_master/TK_veg_master.csv'
@@ -26,10 +27,11 @@ pointblue.palette <-
     '#666666')
 
 # DATA SET UP-------------
-shp_poly <- st_read(here::here('GIS'), poly) 
+shp_poly <- st_read(here::here('GIS'), poly, quiet = TRUE) 
 
-dat <- read_csv(here::here(masterveg)) %>%
+dat <- read_csv(here::here(masterveg), col_types = cols()) %>%
   filter(Year > 2011) %>%
+  mutate(Pasture = as.factor(Pasture)) %>%
   # get pasture area from polygons
   full_join(shp_poly %>% st_set_geometry(NULL), by = 'Pasture') %>%
   # calculate cover area
@@ -38,53 +40,108 @@ dat <- read_csv(here::here(masterveg)) %>%
   filter(!is.na(cover)) %>%
   group_by(Year, vegtype) %>%
   summarize(area_ha = sum(area_ha),
-            cover.area = sum(cover.area, na.rm = T)) %>%
-  mutate(Cover = cover.area / area_ha)
-  
-# test models:
-mod = betareg::betareg(Cover ~ Year * vegtype, dat) 
-summary(mod)
-# significant increase in NativeGr, PereGr
+            cover.area = sum(cover.area, na.rm = T),
+            Cover = cover.area / area_ha) %>%
+  ungroup() %>%
+  mutate(y_round = paste0(round(Cover * 100, digits = 0), '%'),
+         fullname = recode(vegtype, AnnualGr = 'Annual Grasses',
+                           BareGround = 'Bare Ground',
+                           Grass = 'All Grasses',
+                           NativeGr = 'Native Grasses',
+                           PereGr = 'Perennial Grasses',
+                           Weeds = 'Invasive Weeds'))
 
-mod = lm(log(Cover) ~ Year * vegtype, dat) 
-summary(mod)
-# significant increase in NativeGr
 
-dat_final <- dat %>%
-  mutate(Cover = round(Cover * 100, digits = 0),
-         vegtype = recode(vegtype, AnnualGr = 'Annual Grasses',
-                          BareGround = 'Bare Ground',
-                          Grass = 'All Grasses',
-                          NativeGr = 'Native Grasses',
-                          PereGr = 'Perennial Grasses',
-                          Weeds = 'Invasive Weeds'))
+# ESTIMATE TRENDS---------------
+# model ranch-wide trend over time in each major veg class:
+mod = lm(qlogis(Cover) ~ vegtype * Year, 
+         dat %>% filter(!(vegtype %in% c('PereGr', 'NativeGr', 'AnnualGr')))) 
+summary(mod)
+# no significant changes in any major veg class
+
+mod.alt = betareg::betareg(Cover ~ Year * vegtype, 
+                       dat %>% filter(!(vegtype %in% c('PereGr', 'NativeGr', 'AnnualGr')))) 
+summary(mod.alt)
+
+# model ranch-wide trend over time within grasses:
+mod2 = lm(qlogis(Cover) ~ Year * vegtype, 
+          dat %>% filter(vegtype %in% c('PereGr', 'NativeGr', 'AnnualGr')))
+summary(mod2)
+# no significant changes
+
 
 # PLOT 1-------------
 # Overview: all grasses, shrubs, forbs, bare ground, weeds
 
-theme_custom = theme_classic() +
-  theme(
-    legend.title = element_blank(),
-    legend.position = c(1, 1),
-    legend.justification = c(1, 1),
-    legend.text = element_text(size = 10),
-    axis.text = element_text(size = 12),
-    axis.title = element_text(size = 12, vjust = 1, face = 'plain'),
-    axis.line.y = element_line(color = "black"),
-    axis.line.x = element_line(color = "black")
-  )
-
-plot1 <- dat_final %>% 
-  filter(vegtype %in% c(
-    'All Grasses', 'Shrubs', 'Forbs', 'Bare Ground', 'Invasive Weeds')) %>%
-  ggplot(aes(x = Year, y = Cover, color = vegtype)) +
-  geom_line() + geom_point(pch = 19) + ylab('% Cover') + xlab(NULL) +
-  ylim(0, 80) + scale_color_manual(values = pointblue.palette[c(1:4, 6)]) +
-  theme_custom
-
-plot1 <- plot1 %>%
-  plotly::ggplotly() %>% 
-  plotly::layout(legend = list(x = 0, y = 1, tracegroupgap = 5)) 
+plot1 <- plot_ly(x = ~Year) %>%
+  add_trace(data = dat %>% filter(vegtype == 'Grass'), 
+            y = ~Cover * 100, 
+            type = 'scatter', 
+            mode = 'markers+lines',
+            line = list(color = pointblue.palette[1]),
+            marker = list(color = pointblue.palette[1], 
+                          size = 10),
+            text = ~y_round,
+            hoverinfo = 'x+text', 
+            name = 'All Grasses') %>%
+  add_trace(data = dat %>% filter(vegtype == 'BareGround'), 
+            y = ~Cover * 100, 
+            type = 'scatter', 
+            mode = 'lines+markers',
+            line = list(color = pointblue.palette[2]),
+            marker = list(color = pointblue.palette[2], 
+                          size = 10),
+            text = ~y_round,
+            hoverinfo = 'x+text', 
+            name = 'Bare Ground') %>%
+  add_trace(data = dat %>% filter(vegtype == 'Forbs'), 
+            y = ~Cover * 100, 
+            type = 'scatter', 
+            mode = 'lines+markers',
+            line = list(color = pointblue.palette[3]),
+            marker = list(color = pointblue.palette[3], 
+                          size = 10),
+            text = ~y_round,
+            hoverinfo = 'x+text', 
+            name = 'Forbs') %>%
+  add_trace(data = dat %>% filter(vegtype == 'Weeds'), 
+            y = ~Cover * 100, 
+            type = 'scatter', 
+            mode = 'lines+markers',
+            line = list(color = pointblue.palette[4]),
+            marker = list(color = pointblue.palette[4], 
+                          size = 10),
+            text = ~y_round,
+            hoverinfo = 'x+text', 
+            name = 'Invasive Weeds') %>%
+  add_trace(data = dat %>% filter(vegtype == 'Shrubs'), 
+            y = ~Cover * 100, 
+            type = 'scatter', 
+            mode = 'lines+markers',
+            line = list(color = pointblue.palette[6]),
+            marker = list(color = pointblue.palette[6], 
+                          size = 10),
+            text = ~y_round,
+            hoverinfo = 'x+text', 
+            name = 'Shrubs') %>%
+  layout(yaxis = list(title = '% Cover',
+                      font = list(size = 14),
+                      showline = TRUE,
+                      ticks = 'outside',
+                      tick0 = 0,
+                      range = c(0, 80),
+                      showgrid = FALSE),
+         xaxis = list(title = NA,
+                      showline = TRUE,
+                      ticks = 'outside',
+                      showgrid = FALSE),
+         legend = list(x = 0.01, xanchor = 'left', y = 1, yanchor = 'top',
+                       bordercolor = ~I('black'), borderwidth = 1),
+         hovermode = 'x') %>%
+  config(collaborate = FALSE, displaylogo = FALSE, showTips = FALSE,
+         modeBarButtonsToRemove = list('zoom2d', 'select2d', 'lasso2d', 
+                                       'zoomIn2d', 'zoomOut2d', 
+                                       'pan2d', 'toggleSpikelines'))
 
 htmlwidgets::saveWidget(plot1,
                         here::here(graph1),
@@ -94,19 +151,58 @@ htmlwidgets::saveWidget(plot1,
 # PLOT 2-------------
 # Native vs. Perennial vs. Annual Grasses
 
-plot2 <- dat_final %>%
-  filter(vegtype %in% c('Native Grasses', 'Annual Grasses', 'Perennial Grasses')) %>%
-  ggplot(aes(x = Year, y = Cover, color = vegtype)) + 
-  geom_line() + geom_point(pch = 19) + ylab('% Cover') + xlab(NULL) + 
-  ylim(0, 80) + scale_color_manual(values = pointblue.palette[1:3]) +
-  theme_custom
-
-plot2 <-  plot2 %>% 
-  plotly::ggplotly() %>% 
-  plotly::layout(legend = list(x = 0, y = 1, tracegroupgap = 5))
+plot2 <- plot_ly(x = ~Year) %>%
+  add_trace(data = dat %>% filter(vegtype == 'AnnualGr'), 
+            y = ~Cover * 100, 
+            type = 'scatter', 
+            mode = 'markers+lines',
+            line = list(color = pointblue.palette[1]),
+            marker = list(color = pointblue.palette[1], 
+                          size = 10),
+            text = ~y_round,
+            hoverinfo = 'x+text', 
+            name = 'Annual Grasses') %>%
+  add_trace(data = dat %>% filter(vegtype == 'PereGr'), 
+            y = ~Cover * 100, 
+            type = 'scatter', 
+            mode = 'lines+markers',
+            line = list(color = pointblue.palette[3]),
+            marker = list(color = pointblue.palette[3], 
+                          size = 10),
+            text = ~y_round,
+            hoverinfo = 'x+text', 
+            name = 'Perennial Grasses') %>%
+  add_trace(data = dat %>% filter(vegtype == 'NativeGr'), 
+            y = ~Cover * 100, 
+            type = 'scatter', 
+            mode = 'lines+markers',
+            line = list(color = pointblue.palette[2]),
+            marker = list(color = pointblue.palette[2], 
+                          size = 10),
+            text = ~y_round,
+            hoverinfo = 'x+text', 
+            name = 'Native Grasses')%>%
+  layout(yaxis = list(title = '% Cover',
+                      font = list(size = 14),
+                      showline = TRUE,
+                      ticks = 'outside',
+                      tick0 = 0,
+                      range = c(0, 80),
+                      showgrid = FALSE),
+         xaxis = list(title = NA,
+                      showline = TRUE,
+                      ticks = 'outside',
+                      showgrid = FALSE),
+         legend = list(x = 0.01, xanchor = 'left', y = 1, yanchor = 'top',
+                       bordercolor = ~I('black'), borderwidth = 1),
+         hovermode = 'x') %>%
+  config(collaborate = FALSE, displaylogo = FALSE, showTips = FALSE,
+         modeBarButtonsToRemove = list('zoom2d', 'select2d', 'lasso2d', 
+                                       'zoomIn2d', 'zoomOut2d', 
+                                       'pan2d', 'toggleSpikelines'))
 
 htmlwidgets::saveWidget(plot2,
                         here::here(graph2),
                         selfcontained = TRUE,
-                        title = 'TomKat Vegetation Trends')
+                        title = 'TomKat Grass Trends')
 
