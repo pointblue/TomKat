5e4c860e169b0d45777a260773e358f5a8ec0c8b
Kristen Dybala <kdybala@gmail.com>
2019-01-08 15:47:18 -0800

    update weather data

--- code/weather_prep.R
+++ code/weather_prep.R
@@ -5,19 +5,20 @@
 library(tidyverse)
 
 # input files
-rawdat <- 'data_raw/TOKA_WeatherWest_10.23.17_10.23.18.csv'
-olddat <- 'data_raw/TK_weather_master_2017.csv' #data from old weather station
+rawdat <- 'data_raw/TOKA_weatherwest_10.23.17_12.18.18_utf8.csv'
+olddat <- 'data_raw/TOKA_Weather_ALL_9.8.10_7.24.17.csv'
+# olddat <- 'data_raw/TK_weather_master_2017.csv' #data from old weather station
 
 # output files
-masterdat <- 'data_master/TK_weather_master_daily.csv'
-masterdat2 <- 'data_master/TK_weather_master_monthly.csv'
+master_daily <- 'data_master/TK_weather_master_daily.csv'
+master_monthly <- 'data_master/TK_weather_master_monthly.csv'
 
 
 
 # DATA SET UP----------------
 # new data from WeatherWest (from October 2017): hourly data
 #  extract daily min/max temp and rain totals
-dat <- read_csv(here::here(rawdat)) %>%
+dat <- read_csv(here::here(rawdat), col_types = cols()) %>%
   mutate(date = as.Date(Date, format = '%m/%d/%Y')) %>%
   rename(temp.F = `Temp (°F)`,
          temp.max = `Daily Max Temp (°F)`,
@@ -30,17 +31,26 @@ dat <- read_csv(here::here(rawdat)) %>%
 
 # data from old weather station: half-hour data
 #   summarize to extract daily min/max temp and rain totals
-dat2 <- read_csv(here::here(olddat)) %>%
-  select(date.time, date, temp.F, rain.in) %>%
-  # drop to one hour time intervals for comparability
-  mutate(time = format(date.time, '%M')) %>%
-  filter(time == '00') %>%
-  group_by(date) %>%
+dat2 <- read_csv(here::here(olddat), skip = 2, col_types = cols()) %>%
+  filter(WindDir != 'Degree') %>% #drop row of units only
+  rename('date.time' = `Date/Time`,
+         'temp.F' = AirTemp,
+         'rain.in' = DailyRain) %>%
+  select(date.time, temp.F, rain.in) %>%
+  mutate(date.time = as.POSIXct(date.time, format = '%m/%d/%Y %H:%M'),
+         date = as.Date(date.time, '%Y-%m-%d'),
+         time = format(date.time, '%M'),
+         temp.F = as.numeric(temp.F),
+         rain.in = as.numeric(rain.in)) %>%
+  filter(time == '00') %>%  # drop to one hour time intervals for comparability
+  group_by(date) %>% #daily summary
   summarize(n.temp = sum(!is.na(temp.F)),
             temp.max = max(temp.F, na.rm = T),
             temp.min = min(temp.F, na.rm = T),
             n.rain = sum(!is.na(rain.in)),
-            rain.in = max(rain.in, na.rm = T)) %>%
+            rain.in = max(rain.in, na.rm = T),
+            rain.in = case_when(is.infinite(rain.in) ~ NA_real_,
+                                TRUE ~ rain.in)) %>%
   # if fewer than 20 records per day, consider min/max temp and rain unknown
   mutate(temp.max = case_when(n.temp < 20 ~ NA_real_,
                               TRUE ~ temp.max),
@@ -52,7 +62,7 @@ dat2 <- read_csv(here::here(olddat)) %>%
 
 sdat <- bind_rows(dat2, dat)
 
-write_csv(sdat, here::here(masterdat))
+write_csv(sdat, here::here(master_daily))
 
 
 # MONTHLY WEATHER STATS--------------
@@ -77,4 +87,4 @@ mdat <- sdat %>%
   mutate(date = as.Date(paste0(yearmo, '-15'))) %>%
   select(-yearmo, -n.temp, -n.rain)
 
-write_csv(mdat, here::here(masterdat2))
+write_csv(mdat, here::here(master_monthly))
