4aee28fc0d80b93d8cbf783f6b04db57f2671c7a
Kristen Dybala <kdybala@gmail.com>
2019-01-08 16:45:48 -0800

    update CSS

--- code/weather_graph2.R
+++ code/weather_graph2.R
@@ -13,24 +13,21 @@ normals <- 'data_master/TK_weather_estimated_normals.csv'
 graph2 <- 'weather_graph2.html'
 
 
-pointblue.palette <-
-  c('#4495d1',
-    '#74b743',
-    '#f7941d',
-    '#005baa',
-    '#bfd730',
-    '#a7a9ac',
-    '#666666')
+pointblue.palette <- c('#4495d1', '#74b743', '#f7941d', '#005baa',
+                       '#bfd730', '#a7a9ac', '#666666')
 
+tk.palette <- c('#3b4035', '#9c8755', '#61655c',
+                '#d1bc8b', '#40696f', '#2e5150',
+                '#5f5131', '#9e513a')
 
 ##----Monthly difference from long-term average----
-norm <- read_csv(here::here(normals)) %>%
+norm <- read_csv(here::here(normals), col_types = cols()) %>%
   rename(high = TMAX,
          low = TMIN,
          rain = PRCP) %>%
   gather(rain:low, key = 'var', value = 'normals')
 
-dat <- read_csv(here::here(masterdat)) %>%
+dat <- read_csv(here::here(masterdat), col_types = cols()) %>%
   rename(high = temp.max,
          low = temp.min,
          rain = rain.in) %>%
@@ -40,7 +37,7 @@ dat <- read_csv(here::here(masterdat)) %>%
   mutate(diff = observed - normals) %>%
   select(date, month, var, diff) %>%
   # spread(key = var, value = diff) %>%
-  filter(date > '2010-09-15' & date < '2018-10-15') %>% #first and last rows all NA
+  filter(date > '2010-09-15') %>% #first row is all NA
   arrange(date) %>%
   mutate(value2 = case_when(diff > 0 ~ paste0('+', htmlTable::txtRound(diff, digits = 1)),
                             diff <= 0 ~ htmlTable::txtRound(diff, digits = 1)),
@@ -53,61 +50,45 @@ dat <- read_csv(here::here(masterdat)) %>%
 
 # TIME SERIES--------------
 
-hl <- list(bgcolor = 'white',
-           font = list(family = 'Arial'),
-           size = 12,
-           color =  pointblue.palette[7])
-
-plot_ly(x = ~date, y = ~diff) %>%
-  add_trace(data = dat %>% filter(var == 'High'), color = ~I(color), type = 'bar',
-            text = ~text, hoverinfo = 'x+text') %>%
-  add_trace(data = dat %>% filter(var == 'Low'), color = ~I(color), type = 'bar',
-            text = ~text, hoverinfo = 'x+text') %>%
-  add_trace(data = dat %>% filter(var == 'Rain'), color = ~I(color), type = 'bar',
-            text = ~text, hoverinfo = 'x+text') %>%
-  layout(yaxis = list(title = NA),
-         xaxis = list(title = NA),
-         showlegend = F,
-         hovermode = 'x',
-         dragmode = 'pan') %>%
-  config(collaborate = FALSE, displaylogo = FALSE)
-
 p1 <- plot_ly(dat %>% filter(var == 'High'), x = ~date, y = ~diff, type = 'bar',
               color = ~I(color), text = ~text, hoverinfo = 'x+text') %>%
-  layout(yaxis = list(title = 'High (°F)'),
-         xaxis = list(title = NA))
+  layout(yaxis = list(title = 'High (°F)',
+                      font = list(size = 14, family = 'sans-serif')),
+         xaxis = list(title = NA,
+                      font = list(size = 14, family = 'sans-serif')))
 
 p2 <- plot_ly(dat %>% filter(var == 'Low'), x = ~date, y = ~diff, type = 'bar',
               color = ~I(color), text = ~text, hoverinfo = 'x+text') %>%
-  layout(yaxis = list(title = 'Low (°F)'),
-         xaxis = list(title = NA))
+  layout(yaxis = list(title = 'Low (°F)',
+                      font = list(size = 14, family = 'sans-serif')),
+         xaxis = list(title = NA,
+                      font = list(size = 14, family = 'sans-serif')))
 
 p3 <- plot_ly(dat %>% filter(var == 'Rain'), x = ~date, y = ~diff, type = 'bar',
               color = ~I(color), text = ~text, hoverinfo = 'x+text') %>%
-  layout(yaxis = list(title = 'Precipitation (in)'),
-         xaxis = list(title = NA))
+  layout(yaxis = list(title = 'Precipitation (in)',
+                      font = list(size = 14, family = 'sans-serif')),
+         xaxis = list(title = NA,
+                      font = list(size = 14, family = 'sans-serif')))
 
 
 plot2 <- subplot(p1, p2, p3, nrows = 3, shareX = TRUE, titleY = TRUE) %>%
   layout(showlegend = FALSE,
          hovermode = 'x',
          dragmode = 'pan',
-         xaxis = list(rangeselector = list(buttons = list(list(count = 1,
+         xaxis = list(range = c('2010-10-01', '2018-12-31'),
+                      rangeselector = list(buttons = list(list(count = 1,
                                                                label = "YTD",
                                                                step = "year",
                                                                stepmode = "todate"),
-                                                          list(count = 1,
-                                                               label = "1 yr",
-                                                               step = "year",
-                                                               stepmode = "backward"),
                                                           list(count = 2,
                                                                label = "2 yrs",
                                                                step = "year",
                                                                stepmode = "backward"),
                                                           list(step = "all")),
                                            font = list(color = 'white'),
-                                           bgcolor = pointblue.palette[4],
-                                           activecolor = pointblue.palette[1])),
+                                           bgcolor = tk.palette[2],
+                                           activecolor = tk.palette[5])),
          shapes = list(
            list(type = "rect",
                 fillcolor = pointblue.palette[6], line = list(color = "transparent"), opacity = 0.3,
@@ -141,7 +122,10 @@ plot2 <- subplot(p1, p2, p3, nrows = 3, shareX = TRUE, titleY = TRUE) %>%
                 fillcolor = pointblue.palette[6], line = list(color = "transparent"), opacity = 0.3,
                 x0 = "2011-03-01", x1 = "2011-07-01", xref = "x",
                 y0 = 0, y1 = 1, yref = "paper"))) %>%
-  config(collaborate = FALSE, displaylogo = FALSE)
+  config(collaborate = FALSE, displaylogo = FALSE, showTips = FALSE,
+         modeBarButtonsToRemove = list('zoom2d', 'select2d', 'lasso2d', 
+                                       'zoomIn2d', 'zoomOut2d', 
+                                       'pan2d', 'toggleSpikelines'))
 
 htmlwidgets::saveWidget(plot2,
                         here::here(graph2),
