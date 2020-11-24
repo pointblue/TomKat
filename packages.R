a3e5e36c0759b2123d625d655dde818758a0bc69
Kristen Dybala <kdybala@gmail.com>
2020-11-24 15:08:58 -0800

    introduce drake workflow files

---
+++ packages.R
@@ -0,0 +1,27 @@
+# Load all your packages before calling make().
+
+library(drake)
+library(tidyverse)
+library(pbdistance) # devtools::install_github('kdybala/pbdistance')
+library(sf)
+library(plotly)
+library(htmlTable)
+library(leaflet)
+library(mapview)
+library(raster)
+
+conflicted::conflict_prefer("filter", "dplyr")
+conflicted::conflict_prefer("select", "dplyr")
+conflicted::conflict_prefer("gather", "tidyr")
+conflicted::conflict_prefer("arrange_", "plotly")
+conflicted::conflict_prefer("config", "plotly")
+conflicted::conflict_prefer("layout", "plotly")
+
+# library(tibble)
+# # library(qs) #for quicker reading to/from disk
+# # library(fst) #for quicker reading to/from disk
+# library(palo) #remotes::install_github("pointblue/palo")
+
+# conflicted::conflict_prefer("select", "dplyr")
+# conflicted::conflict_prefer("extract", "raster")
+# conflicted::conflict_prefer("lag", "stats")
