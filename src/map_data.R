4d5ca13fbacc0a4567830538ff7fb348ebde4707
Kristen Dybala <kdybala@gmail.com>
2021-12-13 18:11:53 -0800

    reorganize function for clarity

--- src/map_data.R
+++ src/map_data.R
@@ -25,45 +25,35 @@ make_html_tables = function(dat, table.total = FALSE, table.header = NA,
       ))
 }
 
-map_data = function(dat, maplayers = 'single', htmltab = NULL, 
-                    pts_toka, as_raster = FALSE, 
-                    bins = NULL, palette = NULL,
-                    legend.title = NULL, legend.labels = NULL, 
-                    pts_hocr = NULL, fields = NULL, boundary = NULL) {
+# "dat" should have field "var"
+map_data = function(dat, as_raster = FALSE, maplayers = 'single', htmltab = NULL, 
+                    pts_toka, fields = NULL, boundary = NULL,
+                    palette = NULL, bins = NULL, legend.labels = NULL, 
+                    legend.title = NULL, 
+                    pts_hocr = NULL) {
   
-  # turn pc points into raster grid:
-  shp_pts_utm <- read_sf(pts_toka) %>% select(Name) %>% 
-    filter(Name %in% dat$Point) #(drop points not surveyed)
+  # all field data are based on grid of point count stations:
+  shp_pts_utm <- read_sf(pts_toka) %>% select(Point = Name)
   
-  # Note: leave orig version in UTM, to allow setting raster grid size in meters,
-  # but aso create longlat version for mapping, joined to data:
-  shp_pts_longlat <- shp_pts_utm %>% 
-    st_transform(4326) %>% 
-    left_join(dat, by = c('Name' = 'Point'))
+  # convert to longlat version for mapping & join to data to be mapped:
+  # (right join to drop any points not in data)
+  shp_pts_longlat <- shp_pts_utm %>% st_transform(4326) %>% inner_join(dat)
   
-  # add htmltables (if using)
+  # join with code for pop-up htmltables (if using)
   if (!is.null(htmltab)) {
-    shp_pts_longlat <- shp_pts_longlat %>% 
-      inner_join(htmltab, by = c('Name' = 'Point', 'maplayer')) %>% 
-      mutate(maplayer = factor(maplayer, levels = maplayers)) %>% 
-      split(.$var)
+    shp_pts_longlat <- shp_pts_longlat %>% left_join(htmltab)
+  }
+  
+  # color palette------
+  if (is.null(palette)) {
+    palette <- leaflet::colorBin(
+      palette = c('#ffffff', '#005baa'), #white to point blue palette[4]
+      domain = dat$value,
+      bins = bins, #user defined
+      na.color = 'transparent')
   }
 
-  # repeat for pts_hocr (if using)
-  if (!is.null(pts_hocr)) {
-    shp_pts_hocr <- read_sf(pts_hocr) %>% 
-      st_transform(4326) %>%
-      left_join(dat, by = c('Name' = 'Point'))
-    
-    # add htmltables (if using)
-    if (!is.null(htmltab)) {
-      shp_pts_hocr <- shp_pts_hocr %>% 
-        inner_join(htmltab, by = c('Name' = 'Point'))
-    }
-  }
-  
-  
-  # start map base:------------------
+  # start map base------------------
   m <- leaflet(shp_pts_longlat[[1]], height = 500) %>% 
     setView(lng = -122.3598,
             lat = 37.26693,
@@ -76,13 +66,14 @@ map_data = function(dat, maplayers = 'single', htmltab = NULL,
                     url = 'https://www.pointblue.org',
                     width = 174, height = 90, offset.y = -5)
   
-  
-  
-  # ADD RASTERS:-----------------------
-  # create generic raster template that matches the extent and resolution,
-  # placing the point count grid in center of each cell (200 x 200m), then
-  # convert to longlat
+  # add rasters----------
+  # if using (e.g. for showing bird density data as a raster), add any raster
+  # version of data to map first, so it lies under polygons and points
+
   if (as_raster) {
+    # first create generic raster template that matches the extent and
+    # resolution of the point count grid, so that each point count station falls
+    # in the center of each cell (200 x 200m)
     rtemplate <- raster::raster(ncols = 21,
                                 nrows = 13,
                                 resolution = 200,
@@ -91,31 +82,25 @@ map_data = function(dat, maplayers = 'single', htmltab = NULL,
                                 ymn = st_bbox(shp_pts_utm)$ymin - 100,
                                 ymx = st_bbox(shp_pts_utm)$ymax + 100,
                                 crs = st_crs(shp_pts_utm)$proj4string) %>% 
-      raster::projectRaster(crs = crs(shp_pts_longlat))
+      # convert to long-lat
+      raster::projectRaster(crs = st_crs(shp_pts_longlat)$proj4string)
     
     # for each level of "group" in data, transfer richness values from point count
     # coordinates to corresponding raster cell:
-    rast = shp_pts_longlat %>% select(group, Name, value) %>% split(.$group) %>% 
+    rast = shp_pts_longlat %>% select(group, Point, value) %>% split(.$group) %>% 
       purrr::map(function(x) {
         df = as(x, 'Spatial')
         raster::rasterize(x = df, y = rtemplate, field = x$value)
       })
     
-    # color palette:
-    pal.blues <- leaflet::colorBin(
-      palette = c('#ffffff', '#005baa'), #white to point blue palette[4]
-      domain = dat$value, 
-      bins = bins, #user defined
-      na.color = 'transparent')
-    
-    # add rasters 
+    # add rasters to map
     for (i in c(1:length(rast))) {
       m <- m %>% 
         addRasterImage(x = rast[[i]], group = names(rast)[i],
-                       colors = pal.blues, opacity = 0.8, project = F)
+                       colors = palette, opacity = 0.8, project = F)
     }
     
-    # if more than one raster, add layers control:
+    # if more than one raster, also add layers control:
     if (length(rast) > 1) {
       m <- m %>% 
         addLayersControl(position = 'bottomleft', 
@@ -123,17 +108,17 @@ map_data = function(dat, maplayers = 'single', htmltab = NULL,
                          baseGroups = names(rast))
     }
     
-    # add Legend
+    # add raster legend
     m <- m %>% 
       addLegend(position = 'topright', 
-                colors = pal.blues(bins[-length(bins)] + diff(bins)/2), #midpoints
+                colors = palette(bins[-length(bins)] + diff(bins)/2), #midpoints
                 labels = legend.labels,
                 values = NULL,
                 opacity = 1,
                 title = legend.title) 
   }
 
-  # ADD OTHER POLYGONS: (if included)----------------
+  # add boundaries-----------
   ## pasture boundaries
   if (!is.null(fields)) {
     shp_poly <- read_sf(fields) %>% st_transform(4326)
@@ -141,6 +126,7 @@ map_data = function(dat, maplayers = 'single', htmltab = NULL,
       addPolygons(data = shp_poly, fillColor = '#74b743', #point blue green
                   color = 'black', weight = 1, fillOpacity = 0.1)
   }
+  
   ## ranch boundary
   if (!is.null(boundary)) {
     shp_ranch <- read_sf(boundary) %>% st_transform(4326)
@@ -149,23 +135,26 @@ map_data = function(dat, maplayers = 'single', htmltab = NULL,
                   color = 'black', weight = 3)
   }
   
-  # ADD POINTS & POPUP TABLES (if using)---------------
-  # ** Note: add AFTER pasture and ranch boundary polygons so they aren't buried
-  
+  # add points & popups---------------
+
   if (!is.null(htmltab) & as_raster) {
-    # if using rasters, use transparent circle markers, one for each set of
-    # pop-ups to go with each layer in rast
+    # if using rasters, use transparent circle markers to allow pop-up tables, 
+    # one layer for each set of pop-ups to go with any layer in rast
     for (i in c(1:length(rast))) {
       m <- m %>% 
-        addCircleMarkers(popup = ~ table_html,
-                         group = names(rast),
+        addCircleMarkers(data = shp_pts_longlat,
+                         popup = ~table_html,
+                         group = names(rast)[i],
+                         # make them large to cover most of the cell:
                          radius = 11, weight = 1.5, 
                          options = popupOptions(maxWidth = 800),
                          color = 'transparent',
                          fillColor = 'transparent')
     }
+    
   } else if (!is.null(htmltab)) {
     for (i in c(1:length(shp_pts_longlat))) {
+      # match to correct palette for maplayer
       p = which(names(palette) == shp_pts_longlat[[i]]$maplayer[1])
       m <- m %>% 
         addCircleMarkers(data = shp_pts_longlat[[i]],
@@ -182,6 +171,14 @@ map_data = function(dat, maplayers = 'single', htmltab = NULL,
     
   # if more than one maplayers, add layers control and legends to match
   if (length(maplayers) > 1) {
+    
+    # regardless of how many maplayers or types of data will be mapped together,
+    # specify order of maplayers and split data by var (one set of points per data
+    # layer):
+    shp_pts_longlat <-  shp_pts_longlat %>% 
+      mutate(maplayer = factor(maplayer, levels = maplayers)) %>% 
+      split(.$var)
+    
     for (i in c(1:length(maplayers))) {
       m <- m %>% 
         addLegend(pal = palettes[[i]],
@@ -200,29 +197,36 @@ map_data = function(dat, maplayers = 'single', htmltab = NULL,
       hideGroup(maplayers[2:length(maplayers)])
   }
   
-  # ADD HOCR points (if using)--------------
+  # add HOCR points--------------
+  # if using, repeat process to set up data (but won't ever be shown as raster)
   if (!is.null(pts_hocr)) {
+    shp_pts_hocr <- read_sf(pts_hocr) %>% st_transform(4326) %>%
+      select(Point = Name) %>% left_join(dat, by = 'Point')
+    
+    # add htmltables (if using)
     if (!is.null(htmltab)) {
-      # include popup table
+      shp_pts_hocr <- shp_pts_hocr %>% left_join(htmltab, by = 'Point')
+      
       m <- m %>% 
-        addCircleMarkers(data = shp_pts_hocr, popup = ~ table_html,
+        addCircleMarkers(data = shp_pts_hocr, 
+                         popup = ~table_html,
                          radius = 8, weight = 1.5, 
                          options = popupOptions(maxWidth = 800),
                          color = 'black',
-                         fillColor = ~pal.blues(value), 
+                         fillColor = ~palette(value), 
                          fillOpacity = 0.8) 
     } else {
       m <- m %>% 
         addCircleMarkers(data = shp_pts_hocr, 
                          radius = 8, weight = 1.5, 
                          color = 'black',
-                         fillColor = ~pal.blues(value), 
+                         fillColor = ~palette(value), 
                          fillOpacity = 0.8) 
     }
   }
   
-  # MAP FORMAT-------------
-  # add CSS
+  
+  # add CSS-------------
   m$dependencies <- c(m$dependencies,
                       list(
                         htmltools::htmlDependency(
